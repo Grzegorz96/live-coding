76dc2a4991818ffb01995812c35e2d5c9bf27a91
Grzegorz Strzeszewski <grzesstrzeszewski@gmail.com>
2025-08-22 11:55:34 +0200

    3st update.

--- src/binance/binance.service.ts
+++ src/binance/binance.service.ts
@@ -1,7 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 
-type Candles = number[];
+export type Kline = [
+  openTime: number,
+  open: string,
+  high: string,
+  low: string,
+  close: string,
+  volume: string,
+  closeTime: number,
+  quoteAssetVolume: string,
+  numberOfTrades: number,
+  takerBuyBaseAssetVolume: string,
+  takerBuyQuoteAssetVolume: string,
+  ignore: string,
+];
+
+export interface Candle {
+  openTime: number;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  closeTime: number;
+}
 
 @Injectable()
 export class BinanceService {
@@ -9,8 +32,18 @@ export class BinanceService {
 
   async getCandles(symbol: string, interval: string, limit: number) {
     const url = `${this.baseUrl}/klines?symbol=${symbol.toUpperCase()}&interval=${interval}&limit=${limit}`;
-    const { candles } = await axios.get<Candles>(url);
+    const { data } = await axios.get<Kline[]>(url);
 
-    const formated = candles;
+    const candles: Candle[] = data.map((kline) => ({
+      openTime: kline[0],
+      open: parseFloat(kline[1]),
+      high: parseFloat(kline[2]),
+      low: parseFloat(kline[3]),
+      close: parseFloat(kline[4]),
+      volume: parseFloat(kline[5]),
+      closeTime: kline[6],
+    }));
+
+    return candles;
   }
 }
